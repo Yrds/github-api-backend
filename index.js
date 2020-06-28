1b96dff8c70148166f4c67873cbffe9d9421ae95
Yuri Santos <yrds96@protonmail.com>
2020-06-28 20:50:41 -0300

    Fix port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const port = 3000
+const port = 80
 
 const corsOptions = {
     origin: 'http://localhost:3001',
