4cd3df0be53b6784c0c6d741ddaa374ec0f30a38
Yuri Santos <yrds96@protonmail.com>
2020-06-28 20:52:37 -0300

    port use env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const port = 80
+const port = process.env.PORT || 3000
 
 const corsOptions = {
     origin: 'http://localhost:3001',
